e15c18b81000996c6c36f7cfdae754879f5de3fb
Daniil Mikhaylov <www.daniiland@gmail.com>
2025-03-02 11:05:07 +0800

    add: Added success message for login

--- src/shared/appMessages.ts
+++ src/shared/appMessages.ts
@@ -1,6 +1,7 @@
 export const appMessages = {
   requiredField: 'Обязательное поле',
   authError: 'Ошибка авторизации',
+  loginSuccess: 'Успешный вход',
   loginError: 'Ошибка входа',
   createDashboardError: 'Ошибка создания дашборда'
 };
