c173728a6811488e2940c18cf882c34e3e11f43b
Daniil Mikhaylov <www.daniiland@gmail.com>
2025-03-02 11:15:04 +0800

    add: Added searching items by label

--- src/shared/appMessages.ts
+++ src/shared/appMessages.ts
@@ -3,5 +3,6 @@ export const appMessages = {
   authError: 'Ошибка авторизации',
   loginSuccess: 'Успешный вход',
   loginError: 'Ошибка входа',
+  createDashboardSuccess: 'Дашборд успешно создан',
   createDashboardError: 'Ошибка создания дашборда'
 };
